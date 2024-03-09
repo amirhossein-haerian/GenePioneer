7bb14ee417ee3c42f28f2422066c88c2185e0a79
amirho3in <amirhoseinhaerian@gmail.com>
2024-03-09 15:45:24 +0100

    data_loader is ready now

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,6 +1,7 @@
+import json 
+
 from genepioneer import DataLoader
 
 data_loader = DataLoader("Skin");
-data = data_loader.load_tcga_genes()
 
-print(data[0]["name"])
+data_loader.load_go_connected_genes()
