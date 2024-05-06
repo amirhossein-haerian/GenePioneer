7fe5be89b772ea8b6737a7f6f33c79177429ba9f
amirho3in <amirhoseinhaerian@gmail.com>
2024-05-06 10:52:21 +0200

    added an approach for community detection which will be deleted later

--- genepioneer/network_analysis.py
+++ genepioneer/network_analysis.py
@@ -8,7 +8,7 @@ import random
 
 class NetworkAnalysis:
 
-    def __init__(self, cancer_type, features):
+    def __init__(self, cancer_type, features={}):
         self.cancer_type = cancer_type
         
         self.feature_dict = features
@@ -93,28 +93,84 @@ class NetworkAnalysis:
             # Select a random seed node
             seed = random.choice(tuple(unvisited_nodes))
             S = {seed}
-            unvisited_nodes.remove(seed)
+            if seed in unvisited_nodes: 
+                unvisited_nodes.remove(seed)
             T_current = T
             
             # Grow the module
             while T_current > T_low:
+                print(T_current)
                 C_S = self.find_neighborhood(G, S)
                 avg_weight = self.calculate_average_weight(G, S)
                 candidates = [(node, G.nodes[node]['ls_score']) for node in C_S]
                 candidates.sort(key=lambda x: x[1], reverse=True)
-                
+                print(candidates[0])
                 # Decide whether to add the highest-weighted node
                 if candidates and candidates[0][1] > avg_weight:
                     S.add(candidates[0][0])
-                    unvisited_nodes.remove(candidates[0][0])
+                    if candidates[0][0] in unvisited_nodes: 
+                        unvisited_nodes.remove(candidates[0][0])
+                    print("here")
                 else:
                     x = random.uniform(0, 1)
-                    if candidates and x < (candidates[0][1] - avg_weight) / (T_current - avg_weight):
+                    if candidates and x < np.exp((candidates[0][1] - avg_weight) / T_current):
                         S.add(candidates[0][0])
-                        unvisited_nodes.remove(candidates[0][0])
+                        if candidates[0][0] in unvisited_nodes: 
+                            unvisited_nodes.remove(candidates[0][0])
                 
                 T_current *= 0.9
             
-            modules.append(S)
+            modules.append((list(S), avg_weight))
         
         return modules
+    
+    def calculate_intra_module_metrics(self, G, module):
+        avg_ls_score = sum(G.nodes[node]['ls_score'] for node in module) / len(module)
+        # Additional metrics can be included here
+        return avg_ls_score
+
+    def new_algorithm(self, max_size=30):
+        G = nx.read_gml(f"{self.cancer_type}_network_features.gml")
+        
+        modules = []
+        explored_nodes = set()
+        
+        sorted_nodes = sorted(G.nodes(data=True), key=lambda x: x[1]['ls_score'], reverse=True)
+        count = 1
+        for node, data in sorted_nodes:
+            if node not in explored_nodes:
+                new_module = {node}
+                threshold = .8
+                growth_possible = True
+                while growth_possible and len(new_module) < max_size:
+                    growth_possible = False
+                    boundary = self.find_neighborhood(G, new_module)
+                    avg_ls = self.calculate_average_weight(G, new_module)
+                    max_ls = max(G.nodes[node]['ls_score'] for node in boundary) if boundary else 1
+                    max_connectivity = max(sum(1 for neighbor in G.neighbors(n) if neighbor in new_module) for n in boundary)
+
+                    candidate_scores = {}
+                    for node in boundary:
+                        if node not in new_module:
+                            connectivity = sum(1 for neighbor in G.neighbors(node) if neighbor in new_module)
+                            relevance_score = G.nodes[node]['ls_score'] * (connectivity) / max_connectivity
+                            candidate_scores[node] = relevance_score
+
+                    if candidate_scores:
+                        threshold_value = avg_ls * threshold
+                        eligible_candidates = {node: score for node, score in candidate_scores.items() if score > threshold_value}
+
+                        if eligible_candidates:
+                            next_node = max(eligible_candidates, key=eligible_candidates.get)
+                            new_module.add(next_node)
+                            growth_possible = True
+                            
+                avg_weight = self.calculate_average_weight(G, new_module)
+                modules.append((list(new_module), avg_weight))
+                explored_nodes.update(new_module)
+            print(count)
+            count += 1
+                
+        return modules
+        
+    
