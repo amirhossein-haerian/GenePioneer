c49aa0e19e09a44f4b91e62639c62af5eb5e20db
amirho3in <amirhoseinhaerian@gmail.com>
2024-08-18 12:48:04 +0200

    save all

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genepioneer',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python package for identifying essential genes in cancer.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
