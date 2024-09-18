5aeb093dab08c980be08a8e818f7a5b50d0794bf
amirho3in <amirhoseinhaerian@gmail.com>
2024-09-18 11:18:00 +0200

    the README is updated now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genepioneer',
-    version='1.0.1',
+    version='1.0.2',
     description='A Python package for identifying essential genes in cancer.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
